caf1792ec901399be07534c9b9bdd04bb61fdaf1
Sean O'Brien <git@hometownpixel.com>
2022-12-13 17:48:38 -0700

    Initial commit. The beginning!
    - Setup Lume
    - Add initial project structure to make sure things work

---
+++ _config.js
@@ -0,0 +1,30 @@
+import lume from "lume/mod.ts";
+import pug from "lume/plugins/pug.ts";
+import sass from "lume/plugins/sass.ts";
+
+// Basics
+const site = lume({
+	src: "./src",
+	dest: "./dist",
+	location: new URL("https://www.pomorph.com"),
+	server: {
+		port: 1234,
+		open: true
+	}
+});
+
+// Plugins
+site.use(pug());
+site.use(sass({
+	includes: "./src/sass"
+}));
+
+// Static copies
+site.copy("./src/assets/favicons", ".")
+site.copy("./src/assets/fonts", "fonts")
+site.copy("./src/assets/img", "img")
+
+// Load assets
+site.loadAssets([".js"])
+
+export default site;
