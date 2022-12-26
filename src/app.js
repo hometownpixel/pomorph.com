cb5be51442d53d9dc239e4e92a5e03ae35f42197
Sean O'Brien <git@hometownpixel.com>
2022-12-26 16:33:02 -0700

    Allow timer to handle user inputted values
    - Add lots of functionality to app.js. Allow for a user to input time
    in minutes. Values are checked and validated by: 1) not allowing the
    user to enter non-numbers through the UI; 2) by checking the inputted
    values and returning "reasonable" defaults. Add more functionality
    for handling UI states too. Add pause button and rethink logic. Still
    a huge work in progress, but it's at a point where I want to commit
    because it's "stable" and at a state ready for more features
    - Add pause icon to sprites.svg
    - Lots of UI improvements in _app.scss
    - Add styles for selection and hidden and clickable classes in
    _general.scss
    - Update footer styles to accommodate new shadow on timer
    - Add shadow color variable to _variables.scss

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ Sources that were immensely helpful:
 
 - https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
 - https://www.htmlcenter.com/blog/pausing-javascript-timers/
+- https://knplabs.com/en/blog/how2-tips-how-to-restrict-allowed-characters-inside-a-text-input-in-one-line-of-code
 
 ---------------------------------------- */
 
@@ -11,18 +12,11 @@ Sources that were immensely helpful:
 // GLOBAL VARIABLES
 //
 
-// Default times TODO
-let defaultWorkTime = 1;
-let defaultBreakTime = 1;
-let defaultLongBreakTime = 2;
-
-// Temporary times TODO
-let tempWorkTime, tempBreakTime, tempLongBreakTime;
-
+// Times
 let minutes = 1;
-let timerWorkTimeMs = minutes * 60 * 1000; // Convert to milliseconds
+let minutesInMs;
 
-// Timer state/functionality
+// Timer state and functionality
 let timerInterval; // Timer's interval ID
 let timerStartTime; // When timer starts
 let timerFinishTime; // When timer stops
@@ -30,17 +24,96 @@ let timerDurationLeft; // How much time is left (used if paused)
 let timerSessionStarted = false; // Whether session has started
 let timerRunning = false; // Whether timer is running
 
-// Get controls
-let play = document.querySelector(".controls__play");
-let stop = document.querySelector(".controls__stop")
+// Get UI elements
+const timer = document.querySelector(".timer");
+const timerMinutes = document.querySelector(".timer__minutes");
+const timerSeconds = document.querySelector(".timer__seconds");
+const timerInput = document.querySelector(".timer-input");
+const timerInputMinutes = document.querySelector(".timer-input__minutes");
+const play = document.querySelector(".controls__play");
+const pause = document.querySelector(".controls__pause");
+const stop = document.querySelector(".controls__stop");
 
 //
 // FUNCTIONS
 //
 
+// HELPERS //
+
+function hide(selector) {
+	selector.classList.add("hidden");
+}
+
+function show(selector) {
+	selector.classList.remove("hidden");
+}
+
+function clickable(selector, bool) {
+	// If true
+	if (bool) {
+		selector.classList.add("clickable");
+	}
+	else {
+		selector.classList.remove("clickable");
+	}
+}
+
+function minutesToMs(minutes) {
+	return minutes * 60 * 1000;
+}
+
+// INITIAL LOAD //
+
+function initialLoad() {
+	// When page is first loaded, make timer clickable
+	clickable(timer, true);
+
+	// Add time to timer interface
+	timerMinutes.innerHTML = ("0" + minutes).slice(-2);
+	timerSeconds.innerHTML = "00";
+}
+
+
+// TIMER //
+function decideTime(selector) {
+	// Get time value
+	let t = selector.value;
+
+	// Check if value exists
+	if (t) {
+		// Try to convert to int
+		t = parseInt(t);
+
+		// Check if value is not a number
+		if (isNaN(t)) {
+			// It's not, so return default time
+			return minutes;
+		}
+		// It is a number...
+		else {
+			// If number is too big or small, return "reasonable" defaults
+			if (t < 1) {
+				return 1;
+			}
+			else if (t > 55) {
+				return 55;
+			}
+			// Number seems to be fine, so return it
+			else {
+				return t;
+			}
+		}
+	}
+	// Value doesn't exist, so return default
+	else {
+		return minutes;
+	}
+}
+
 function timeRemaining(finishTime) {
 	// Calculate time remaining by subtracting finish time from current time
 	const total = Date.parse(finishTime) - Date.parse(new Date());
+
 	// Convert to seconds and minutes
 	const seconds = Math.floor((total / 1000) % 60);
 	const minutes = Math.floor((total / 1000 / 60) % 60);
@@ -54,11 +127,6 @@ function timeRemaining(finishTime) {
 
 function startTimer(finishTime) {
 	timerRunning = true;
-
-	// Grab necessary timer elements
-	const timer = document.querySelector(".timer");
-	const timerMinutes = document.querySelector(".timer__minutes");
-	const timerSeconds = document.querySelector(".timer__seconds");
 
 	function updateTimer() {
 		// Calculate remaing time
@@ -74,12 +142,19 @@ function startTimer(finishTime) {
 			clearInterval(timerInterval)
 
 			// Make sure to reset time to original value
-			timerWorkTimeMs = minutes * 60 * 1000;
+			minutesInMs = minutesToMs(minutes);
+
+			// Update UI
+			hide(pause);
+			show(play);
+			hide(stop);
+			clickable(timer, true);
 		}
 	}
 
 	// Run first to avoid delay
 	updateTimer();
+
 	// Execute every second
 	timerInterval = setInterval(updateTimer, 1000);
 }
@@ -90,9 +165,10 @@ function pauseTimer() {
 	clearInterval(timerInterval);
 
 	// Calculate how much time has passed between now and the start of the timer
-	timerDurationLeft = timerWorkTimeMs - (Date.parse(new Date()) - timerStartTime);
+	timerDurationLeft = minutesInMs - (Date.parse(new Date()) - timerStartTime);
+
 	// Make sure to update time, otherwise future calculations will be off
-	timerWorkTimeMs = timerDurationLeft;
+	minutesInMs = timerDurationLeft;
 }
 
 function resumeTimer() {
@@ -106,18 +182,19 @@ function resumeTimer() {
 }
 
 function stopTimer() {
-	// Check if timer is not running
-	if (!timerSessionStarted) {
-		return;
-	}
-	else {
-		// It is, so stop timer and reset session
+	// Check if timer session has started
+	if (timerSessionStarted) {
+		// There is a session, so stop timer (just in case it isn't stopped) and reset session
 		timerRunning = false;
 		timerSessionStarted = false;
 		clearInterval(timerInterval);
 
 		// Make sure to reset time to original value
-		timerWorkTimeMs = minutes * 60 * 1000;
+		minutesInMs = minutesToMs(minutes);
+	}
+	else {
+		// No session, so don't do anything
+		return;
 	}
 }
 
@@ -125,25 +202,66 @@ function stopTimer() {
 // EVENTS
 //
 
+// Timer
+timer.addEventListener("click", function() {
+	// Check if the session has started
+	if (timerSessionStarted) {
+		// Don't do anything if it has
+		return;
+	}
+	// The session has not started so allow the user to input time
+	else if (!timerSessionStarted) {
+		// Update UI
+		hide(timer);
+		show(timerInput);
+		clickable(timerInput, true);
+		timerInputMinutes.focus();
+	}
+});
+
+// Input
+timerInputMinutes.addEventListener("input", function() {
+	// Replace any character that is not a number
+	// Adapted from: https://knplabs.com/en/blog/how2-tips-how-to-restrict-allowed-characters-inside-a-text-input-in-one-line-of-code
+	timerInputMinutes.value = timerInputMinutes.value.replace(/[^0-9+]/g, "");
+});
+
+// Start
 play.addEventListener("click", function() {
 	// Prevent "double" running of the function by checking if a session has already been started
 	if (!timerSessionStarted) {
+		// Decide time
+		minutes = decideTime(timerInputMinutes);
+
 		// Start session
 		timerSessionStarted = true;
 
-		// Calculate finish time
+		// Calculate finish time and start timer
 		timerStartTime = Date.parse(new Date());
-		timerFinishTime = new Date(timerStartTime + timerWorkTimeMs);
-
+		minutesInMs = minutesToMs(minutes);
+		timerFinishTime = new Date(timerStartTime + minutesInMs);
 		startTimer(timerFinishTime);
-	}
-	// Pause
-	else if (timerRunning) {
-		pauseTimer();
+
+		// Update UI
+		// Hide timer input (in case it was activated) and show timer that can no longer be clicked
+		hide(timerInput);
+		show(timer);
+		clickable(timer, false);
+
+		// Hide play button and show pause
+		hide(play);
+		show(pause);
+
+		// Since the session has started, show stop
+		show(stop);
 	}
 	// Resume
 	else if (!timerRunning) {
 		resumeTimer();
+
+		// Update UI
+		hide(play);
+		show(pause);
 	}
 	// Prevent "double" running
 	else {
@@ -151,4 +269,44 @@ play.addEventListener("click", function() {
 	}
 });
 
-stop.addEventListener("click", stopTimer);
+// Pause
+pause.addEventListener("click", function() {
+	// Check if timer is running
+	if (timerRunning) {
+		pauseTimer();
+
+		// Update UI
+		hide(pause);
+		show(play);
+	}
+	// Prevent "double" running
+	else {
+		return;
+	}
+});
+
+// Stop
+stop.addEventListener("click", function() {
+	stopTimer();
+
+	// Update UI
+	// Show play button in case the timer was playing when stopped
+	hide(pause);
+	show(play);
+
+	// Hide stop since the session has been terminated
+	hide(stop);
+
+	// Reset timer interface values
+	timerMinutes.innerHTML = ("0" + minutes).slice(-2);
+	timerSeconds.innerHTML = "00";
+
+	// Make clickable
+	clickable(timer, true);
+});
+
+//
+// RUN
+//
+
+initialLoad();
